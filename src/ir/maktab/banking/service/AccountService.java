130f73d01f785bdd0f6fae5c28ceb4352aea6354
zahra.rahimi <zahra.rahimi@gmail.com>
2022-09-01 17:18:43 +0430

    create withdraw and deposit for longterm account

--- src/ir/maktab/banking/service/AccountService.java
+++ src/ir/maktab/banking/service/AccountService.java
@@ -1,4 +1,10 @@
 package ir.maktab.banking.service;
 
+import ir.maktab.banking.model.CreditCard;
+import ir.maktab.banking.model.Customer;
+import ir.maktab.banking.model.accounts.Account;
+
 public interface AccountService {
+    boolean withdraw(Customer customer,double amount);
+    boolean deposite(Customer customer, double amount);
 }
--- src/ir/maktab/banking/service/LongerAccountService.java
+++ src/ir/maktab/banking/service/LongerAccountService.java
@@ -1,4 +1,19 @@
 package ir.maktab.banking.service;
 
-public class LongerAccountService {
+import ir.maktab.banking.model.Customer;
+
+public class LongerAccountService implements AccountService{
+    Lon
+    @Override
+    public boolean withdraw(Customer customer, double amount) {
+
+        if()
+        if(customer.calCustomerBalance() > amount)
+        return false;
+    }
+
+    @Override
+    public boolean deposite(Customer customer, double amount) {
+        return false;
+    }
 }
