4dc47a1e918288ac76622eb9c3fb072a8bd045a1
se.damavandi@gmail.com <se.damavandi@gmail.com>
2022-09-02 11:30:24 +0430

    d

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import ir.maktab.banking.service.*;
 public class Main {
     public static void main(String[] args) {
         CreditCard creditCard = new CreditCard("6362141092542787", 10000000);
-        //Date date = Date.valueOf("2021-01-01");
 
         CurrentAccount currentAccount = new CurrentAccount("100002111092876000001113", creditCard, "check1");
         LongTermAccount longTermAccount = new LongTermAccount("100002111092876000002243", creditCard, 0.14, 5);
